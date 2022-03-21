436a4ee68cd174b5f9d359b19b415707bb13553a
Mohammedsannan <97687050+Mohammedsannan@users.noreply.github.com>
2022-03-21 14:14:44 +0530

    Add files via upload

---
+++ cube.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class cube {
+
+public static void main(String args[])
+{
+Scanner s= new Scanner(System .in);
+System.out.println("enter your number:");
+int c=s.nextInt();
+for(int i=0;i<=c;i++)
+{
+int m=i*i*i;
+System.out.println(m);
+}
+}
+}
+
